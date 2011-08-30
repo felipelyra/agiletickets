c08e7c3a491ecdecd4612edc6decffcc3b05d8f7
Felipe <felipe.lyra@acotel.com>
2011-08-30 20:26:30 -0300

    Resolve bug para reserva na sessao

--- src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
@@ -30,4 +30,12 @@ public class SessaoTest {
 		sessao.reserva(3);
 		Assert.assertEquals(2, sessao.getIngressosDisponiveis().intValue());
 	}
+	
+	@Test
+	public void deveVender1ingressoSeHa1vaga() throws Exception {
+		Sessao sessao = new Sessao();
+		sessao.setTotalIngressos(1);
+		
+		Assert.assertTrue(sessao.podeReservar(1));
+	}
 }
